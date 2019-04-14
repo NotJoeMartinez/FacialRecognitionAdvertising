5ba07369851142f6a4d908bcaf342a589134abb1
Joseph Martinez <39905973+NotJoeMartinez@users.noreply.github.com>
2019-04-14 09:51:08 -0500

    Removed duplicate firebase

--- app/app.js
+++
@@ -1,9 +0,0 @@
-var evtSource = new EventSource('01.py');
-var eventList = document.querySelector('ul');
-evtSource.onmessage = function(e) {
-    var newElement = document.createElement("li");
-  
-    newElement.textContent = "message: " + e.data;
-    eventList.appendChild(newElement);
-  }
-
